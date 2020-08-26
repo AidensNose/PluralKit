a8c43d42028986367071a0de916cf4ee8e29e93a
AidensNose <70245591+AidensNose@users.noreply.github.com>
2020-08-26 02:22:21 +0100

    Update Fun.cs
    Show your appreciation

--- PluralKit.Bot/Commands/Fun.cs
+++ PluralKit.Bot/Commands/Fun.cs
@@ -5,7 +5,7 @@ namespace PluralKit.Bot
     public class Fun
     {
         public Task Mn(Context ctx) => ctx.Reply("Gotta catch 'em all!");
-        public Task Fire(Context ctx) => ctx.Reply("*A giant lightning bolt promptly erupts into a pillar of fire as it hits your opponent.*");
+        public Task UwU(Context ctx) => ctx.Reply("*Y'all are beautiful and I love you 🥺👉👈*");
         public Task Thunder(Context ctx) => ctx.Reply("*A giant ball of lightning is conjured and fired directly at your opponent, vanquishing them.*");
         public Task Freeze(Context ctx) => ctx.Reply("*A giant crystal ball of ice is charged and hurled toward your opponent, bursting open and freezing them solid on contact.*");
         public Task Starstorm(Context ctx) => ctx.Reply("*Vibrant colours burst forth from the sky as meteors rain down upon your opponent.*");
